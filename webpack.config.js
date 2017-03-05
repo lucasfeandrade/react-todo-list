9066cf3244d395d28ea68d00033aaec682c725cd
Lucas Fernandes Andrade <lucasfeandrade@gmail.com>
2017-03-05 00:53:45 -0300

    Added configuration for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,27 @@
-
+var webpack = require('webpack')
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: ['./client/client.js'],
+    output: {
+        path: './dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.optmize.OccurrenceOrderPlugin(),
+        new webpack.HotModuleReplacementPludin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015', 'react-hmre']
+                }
+            }
+        ]
+    }
+}
